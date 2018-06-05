4020f988719294e7b2eae746a147e95af04088b4
John Samuel <johnsamuelwrites@gmail.com>
2018-06-05 16:56:59 +0200

    Write version metadata

--- src/version.py
+++ src/version.py
@@ -34,3 +34,25 @@ def get_version_wrt_time(past, time):
           print("  start: " + versiondata["existencestarttime"])
           print("  end: " + versiondata["existenceendtime"])
           print("  label: " + versiondata["label"])
+
+def write_version(identifier, startime, endtime, storetransactionstarttime, 
+        storetransactionendtime, 
+        title, description=None, tag=None, document=None, userid=None):
+  version = dict()
+  version["identifier"] = str(identifier)
+  version["existencestarttime"] = startime
+  version["existenceendtime"] = endtime
+  version["storetransactionstarttime"] = storetransactionstarttime
+  version["storetransactionendtime"] = storetransactionendtime
+  version["title"] = title
+  version["description"] = description
+  version["tag"] = tag
+  version["document"] = document
+  version["userid"] = userid.name
+  with open(".urbanco2fab/versions.json", "r") as jsonfile:
+    versions = json.load(jsonfile)
+    versions[str(identifier)] = version
+    jsonfile.close()
+  with open(".urbanco2fab/versions.json", "w") as jsonfile:
+    json.dump(versions, jsonfile,  indent=4, sort_keys=True) 
+    jsonfile.close()
