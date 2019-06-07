ac7e767b332d6a2a2230865f5b67896176d5983d
John Samuel <johnsamuelwrites@gmail.com>
2019-06-07 09:53:26 +0200

    Add function to validate instances

--- urbanco2fab/validate.py
+++ urbanco2fab/validate.py
@@ -28,3 +28,26 @@ class Validate:
         jschema =  json.load(jshandle)
         validate(instance=jsondata, schema=jschema)
 
+  """
+   Validate whether an instance belongs to the required class. 
+   It takes as input a reference class and the instance to validate 
+
+   Parameters
+   ----------
+   referenceclass: str
+     reference class 
+   instancetovalidate: str
+     the instance to validate 
+
+   Returns
+   -------
+     None
+
+   Raises
+   ------ 
+     Exception
+       if they do not match
+  """
+  def validateclass(referenceclass, instancetovalidate):
+    if (instancetovalidate.__class__ != referenceclass):
+      raise Exception("The instance " + str(instancetovalidate.__class__) + " is not of the required class :" + str(referenceclass.__class__))
