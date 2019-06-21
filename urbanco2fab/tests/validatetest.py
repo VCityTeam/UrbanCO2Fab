a5e880c1da3eb42f62947b00b09c91397dc455ea
John Samuel <johnsamuelwrites@gmail.com>
2019-06-21 16:01:58 +0200

    Add parser

--- urbanco2fab/tests/validatetest.py
+++ urbanco2fab/tests/validatetest.py
@@ -7,9 +7,9 @@ from validate import Validate
 
 class ValidationTestSuite(unittest.TestCase):
   def test_basic_validation(self):
-    Validate.validatejsonfile("../../schema/urbanCo2Fab/workspace.schema.json","workspace.json")
-    Validate.validatejsonfile("../../schema/urbanCo2Fab/scenarios.schema.json","scenarios.json")
-    Validate.validatejsonfile("../../schema/urbanCo2Fab/versions.schema.json","versions.json")
+    Validate.validatejsonfile("../schema/urbanCo2Fab/workspace.schema.json","tests/workspace.json")
+    Validate.validatejsonfile("../schema/urbanCo2Fab/scenarios.schema.json","tests/scenarios.json")
+    Validate.validatejsonfile("../schema/urbanCo2Fab/versions.schema.json","tests/versions.json")
 
 if __name__ == '__main__':
   unittest.main()
