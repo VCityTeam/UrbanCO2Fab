a8bbe5e4bdc364cb7166ad8b0936e5bc43812e17
ChataingT <38420068+ChataingT@users.noreply.github.com>
2020-02-12 09:54:07 +0100

    ADD logger config

---
+++ urbanco2fab/logger/logger.py
@@ -0,0 +1,35 @@
+import logging
+
+
+def set_logger(level=logging.DEBUG):
+    format = '%(levelname)s - %(asctime)s - name:%(name)s - file:%(module)s.py - %(message)s'
+
+    # create logger
+    logger = logging.getLogger('root')
+    logger.setLevel(level)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+
+    # create formatter
+    formatter = logging.Formatter(format)
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+
+
+if __name__ == "__main__":
+    set_logger()
+    # 'application' code
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warn('warn message')
+    logger.error('error message')
+    logger.critical('critical message')
+
+ 
