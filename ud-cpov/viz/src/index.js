8764c8b43a4fa96fc97417fb388ddfd905464a85
ChataingT <38420068+ChataingT@users.noreply.github.com>
2020-03-11 15:28:37 +0100

    viz graph working, missing outside json

--- ud-cpov/viz/src/index.js
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-
-var app = express();
-
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
-
-app.listen(3000);
--- ud-cpov/viz/src/viz.js
+++ ud-cpov/viz/src/viz.js
@@ -2,54 +2,75 @@ import * as vis from 'vis-network';
 
 import { get_data_and_options } from "./json_parser";
 
+class NetworkManagerSingleton {
 
-var network = null;
-var data = null;
-var list_options = null;
-var mode = document.getElementById("mode");
+    constructor(start_mode="default", id_network="mynetwork", id_button_location="mybuttons"){
+    /**
+     * Constructor for singleton
+     */
+        const instance = this.constructor.instance;
+        if(instance){
+            return instance;
+        }
+        else{
+            this.network = null;
+            this.data = null;
+            this.list_option = null;
+            this.current_mode = start_mode;
+            this.id_network = id_network;
+            this.id_button_location = id_button_location;
+            this.constructor.instance = this;
+        }
 
-
-function destroy() {
-    if (network !== null) {
-        console.info("Network destroyed");
-      network.destroy();
-      network = null;
     }
-  }
 
-function get_option(list_options, mode){
-    console.log("Get option : %s", mode);
-    var test = mode;
-    if (mode === "default"){
-        test = undefined
-    }
-    for (const key in list_options) {
-        if (list_options.hasOwnProperty(key)) {
-            const element = list_options[key];
-            if (test === element.label){
-                return element.option
-            }
+    destroy() {
+    /**
+     * Kill the simulation network  
+     */
+        if (this.network !== null) {
+            console.info("Network destroyed");
+          this.network.destroy();
+          this.network = null;
         }
     }
-    return list_options[0].option;
-}
 
-function draw(json_path, id_network="mynetwork", id_button_location="mybutton") {
-    destroy();
-    var mode = document.getElementById("mode");
-
-
-    if (data === null){
-        data = get_data_and_options(json_path).data;
+    get_option(mode){
+    /**
+     * Extract the matching options to a specific mode from the list_option
+     * /!\ Working specificaly with vis.network
+     * @param mode specific mode correspondong to a specific view
+     * @return ready to used json for vis-network
+     */
+        console.log("Get option : %s", mode);
+        var test = mode;
+        if (mode === "default"){
+            test = undefined
+        }
+        console.log(1);
+        for (const key in this.list_option) {
+            console.log(1);
+            if (this.list_option.hasOwnProperty(key)) {
+                console.log(1);
+                const element = this.list_option[key];
+                if (test === element.label){
+                    console.log(2);
+                    return element.option.viz
+                }
+            }
+        }
+        return this.list_option[0].option.viz;
     }
-    if (list_options === null){
-        list_options = get_data_and_options(json_path).list_options;
-        
-        // add button for each mode
-        console.log("Try to add %s button(s)", list_options.length.toString());
-        for (const key in list_options) {
-            if (list_options.hasOwnProperty(key)) {
-                const option = list_options[key];
+
+    add_button_to_view(){
+    /**
+     * Add button to navigate between view. There will be one button by options present in list_option
+     * onClick will be a draw()
+     */
+        console.log("Try to add %s button(s)", this.list_option.length.toString());
+        for (const key in this.list_option) {
+            if (this.list_option.hasOwnProperty(key)) {
+                const option = this.list_option[key];
 
                 var button = document.createElement("button");
 
@@ -57,13 +78,15 @@ function draw(json_path, id_network="mynetwork", id_button_location="mybutton") 
                 console.log("Creating button with label : %s", new_label.toString());
                 var label = document.createTextNode(new_label.toString());
                 button.appendChild(label);
+
                 button.onclick = function (param) {
                     console.log("Button %s clicked : %o", param.target.innerText, param);
-                    mode.value = param.target.innerText;
-                    draw('');
+                    var n = new NetworkManagerSingleton();
+                    n.current_mode = param.target.innerText;
+                    n.draw();
                 };
 
-                var element = document.getElementById("mybuttons");
+                var element = document.getElementById(this.id_button_location);
                 element.insertBefore(button, mode);
 
                 console.info("Button added : %s", mode.value);
@@ -71,21 +94,34 @@ function draw(json_path, id_network="mynetwork", id_button_location="mybutton") 
         }
     }
 
-    //var options = list_options[0].option;
-    var options = get_option(list_options, mode.value)
-    var container = document.getElementById(id_network);
-    console.log("Drawing Container : %o Data : %o Options : %o", container, data, options.viz);
-    var network = new vis.Network(container, data, options.viz);
-    console.info("Network drawn");
-    console.log(network);
-    }
-
-
+    draw() {
+    /**
+     * Draw the network inside the client page
+     */
+        this.destroy();  
+    
+        if (this.data === null){
+            this.data = get_data_and_options(json_path).data;
+        }
+        if (this.list_option === null){
+            this.list_option = get_data_and_options(json_path).list_options;
+            this.add_button_to_view();
+        }
+    
+        const options = this.get_option(this.current_mode);
+        const container = document.getElementById(this.id_network);
+        console.log("Drawing Container : %o Data : %o Options : %o", container, this.data, options);
+        var network = new vis.Network(container, this.data, options);
+        console.info("Network drawn");
+        console.log(network);
+        }
+}
 
 // test
-const json_path = __dirname + '/input/urban_graph.json';
+const json_path = __dirname + "/input/urban2.json";
 
 window.addEventListener("load", () => {
     console.log(__dirname);
-    draw(json_path);
+    var n = new NetworkManagerSingleton();
+    n.draw();
     });
